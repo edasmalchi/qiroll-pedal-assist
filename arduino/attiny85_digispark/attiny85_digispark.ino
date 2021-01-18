14b7da6e262d7d317846b135bcb261c84fbf0a8a
Eric Dasmalchi <edasmalchi@gmail.com>
2021-01-17 16:50:02 -0800

    improve code legibility

--- arduino/attiny85_digispark/attiny85_digispark.ino
+++ arduino/attiny85_digispark/attiny85_digispark.ino
@@ -1,5 +1,5 @@
     /*
-      Digispark (ATTINY85) PAS System for Qiroll ebike kit
+      Digispark (ATTINY85) Pedal Assist System for Qiroll ebike kit
       
      (implemented on a "Ximimmark" clone: https://smile.amazon.com/gp/product/B07KVS4YGQ/, 
      note clone has P5 set to reset so can't be used... )
@@ -7,7 +7,7 @@
      Functionality:
      
       - steady output when PAS hall sensor input is pulsing
-      - starts in "off" state, button toggles
+      - starts with PAS off, button toggles
       - double-click button within one second to change controller mode (eco<->power)
           (note that very fast (<175ms) double-clicks may be seen as a bounce and not register)
      
@@ -21,22 +21,22 @@
 
     */
      
-    // constants won't change. They're used here to set pin numbers:
-    const byte buttonPin = 2;   // the number of the pushbutton pin
-    const byte ledPin = 1;      // the number of the LED pin (placeholder for throttle input)
-    const byte hall_Sensor=0;
-    const byte modeChange=4; //pin to eventually change controller mode...
+    // set pin numbers:
+    const byte buttonPin = 2;   // pushbutton pin
+    const byte pasSensorPin = 0; // pas hall sensor input pin
+    // const byte brakeSensorPin = 3; // brake lever hall sensor input pin (option)
+    const byte controllerThrottlePin = 1;      //pin connected to controller throttle input (also board LED)
+    const byte controllerModeChangePin = 4; //pin to change controller mode (external LED placeholder)
      
-    // Variables will change:
-    boolean pasState = false;         // is pedal assist (pas) active?  (false=no pas)
+    // declare variables:
+    boolean pasState = false;         // is pedal assist active?
     boolean lastpasState = false;     // was pedal assist active?
 
     unsigned long lastChangeTime = 0;  // the last time the pas state was changed
-    unsigned long changeDelay = 1000;  // "double-click" timeout, (double-click changes controller mode)
+    unsigned long changeDelay = 1000;  // "double-click" timeout in ms, (double-click changes controller mode)
 
     unsigned long pulseDuration = 0;
     unsigned long pulseThreshold = 400000; // 400,000us = 400ms (for bench testing)
-
     // TODO replace bench testing value once on bike
     // bike value estimate: 2000ms/24 = 83.3ms = 83,333us (approx duration of each low(or high) pulse at 30rpm) (12 highs, 12 lows so 24 pulses/rev)
     // low/high pulses seem approx. equal duration on my 12-magnet sensor. 
@@ -46,12 +46,12 @@
       //interrupt 0 is P2(buttonPin), at least on attiny85 digispark/clones
       attachInterrupt(0, ISR_pasState, CHANGE);
       
-      pinMode(ledPin, OUTPUT);
-      pinMode(hall_Sensor, INPUT_PULLUP);
-      pinMode(modeChange, OUTPUT);
+      pinMode(controllerThrottlePin, OUTPUT);
+      pinMode(pasSensorPin, INPUT_PULLUP);
+      pinMode(controllercontrollerModeChangePinPin, OUTPUT);
 
-      digitalWrite(modeChange, LOW);
-      digitalWrite(ledPin, LOW);
+      digitalWrite(controllerModeChangePin, LOW);
+      digitalWrite(controllerThrottlePin, LOW);
     }
      
     void loop() {
@@ -60,29 +60,29 @@
      if (pasState != lastpasState){
 
        if ((millis() - lastChangeTime) < changeDelay){
-        // placeholder action, will eventually be used to bring pin low when connected to Qiroll controller
-         digitalWrite(modeChange, HIGH);
+        // placeholder action flashing LED, will be used to bring pin low when connected to Qiroll controller
+         digitalWrite(controllerModeChangePin, HIGH);
          delay(500);
-         digitalWrite(modeChange, LOW);
+         digitalWrite(controllerModeChangePin, LOW);
        }
       lastChangeTime = millis();
       lastpasState = pasState;
      }
      
-      // Main pulse timing block!
-      
+      // measure pulse durations from pas sensor to turn throttle on/off
       if (pasState){  
         
-        pulseDuration = pulseIn(hall_Sensor, LOW); 
+        pulseDuration = pulseIn(pasSensorPin, LOW); 
         if (pulseDuration != 0 && pulseDuration <  pulseThreshold){
-          digitalWrite(ledPin, HIGH);   // set the LED on if pulses short enough
+          //TODO switch all highs/lows once installed on bike (controller turns on when low)
+          digitalWrite(controllerThrottlePin, HIGH);   // set the LED on if pulses short enough
         }
         else{
-          digitalWrite(ledPin, LOW);    // set the LED off if pulses too long
+          digitalWrite(controllerThrottlePin, LOW);    // set the LED off if pulses too long
         }
      }
-     else{
-      digitalWrite(ledPin, LOW);  // set the LED off if pas is turned off while LED on
+      else{
+        digitalWrite(controllerThrottlePin, LOW);  // set the LED off if pas is turned off while LED on
      }
 
     }
@@ -96,7 +96,7 @@
        // NOTE test bounce with Qiroll included switch; 175 appropriate for cheap breadboard pushbutton
        if (interrupt_time - last_interrupt_time > 175)
        {
-          pasState = !pasState;
+          pasState = !pasState; //turn pas on/off
        }
        last_interrupt_time = interrupt_time;
       }
